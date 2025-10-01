ad70ea35f135a7989b08f9b5256934f7b5b758bb
Eldar-86 <223857862+Eldar-86@users.noreply.github.com>
2025-10-01 20:27:00 +0200

    Create test_bank.py
    module test for bank.py

---
+++ bank/test_bank.py
@@ -0,0 +1,20 @@
+from bank import value
+
+
+def test_value1():
+    assert value("Hello") == 0
+
+def test_value2():
+    assert value(" Hello") == 0
+
+def test_value3():
+    assert value("Hello, Newman") == 0
+
+def test_value4():
+    assert value("How you doing?") == 20
+
+def test_value5():
+    assert value("What's happening?") == 100
+
+def test_value6():
+    assert value("What's up?") == 100
